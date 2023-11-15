b48dd5aee7965c9243daa218939edef92a5bb441
Jeff Hofstad <dzy929076300@gmail.com>
2023-11-15 10:51:09 +0800

    Initial commit

---
+++ i18n/lang/common.en.ts
@@ -0,0 +1,30 @@
+const translation = {
+  api: {
+    success: 'Success',
+    saved: 'Saved',
+    create: 'Created',
+  },
+  operation: {
+    confirm: 'Confirm',
+    cancel: 'Cancel',
+    clear: 'Clear',
+    save: 'Save',
+    edit: 'Edit',
+    refresh: 'Restart',
+    search: 'Search',
+    send: 'Send',
+    copy: 'Copy',
+    lineBreak: 'Line break',
+    like: 'like',
+    dislike: 'dislike',
+    download: 'Download',
+  },
+  unit: {
+    char: 'chars',
+  },
+  actionMsg: {
+    copySuccessfully: 'Copied successfully',
+  },
+}
+
+export default translation
